b033dded8522bf1522bf32433ccc118d0564f06b
Josh <mrmyers@fullsail.edu>
2013-10-10 19:47:25 -0400

    Dog Years 1

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,13 @@
+//Josh Myers, October 10, 2013, Expressions Worksheet
+
+
+//Dog Years
+
+var sparkyAge = 5; // Given variable
+var dogYears = sparkyAge * 7; 
+
+console.log("Sparky is" + " " + sparkyAge + " " + "human years old, which is" + " " + dogYears + + " " + + "in dog years.")
+
+
+
+
