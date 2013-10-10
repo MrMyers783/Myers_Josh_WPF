efc3d6795686ffaa9837f99e5bb321736634e7cd
Josh <mrmyers@fullsail.edu>
2013-10-10 19:34:20 -0400

    Assigning operators

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,10 +1,12 @@
-//Expressions Casting
+//Expressions Assigning Operators
 
-var areaCode = 413
-var firstPart = 561;
-var secondPart = 6185; 
+var a = 3
 
-var phoneNo = String(areaCode) + String(firstPart) + String(secondPart);
-console.log(phoneNo)
+a ++; 
+
+console.log (a) 
+
+
+
 
 
