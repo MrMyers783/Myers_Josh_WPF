30357d464ded4e454fa596f485ea381ef9c09410
Josh <mrmyers@fullsail.edu>
2013-10-10 19:55:15 -0400

    slice of pie

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -9,5 +9,21 @@ var dogYears = sparkyAge*7; //result variable
 console.log("Sparky is" + " " + sparkyAge + " " + "human years old, which is" + " " + dogYears  + " " +  "in dog years.")
 
 
+//Slice of Pie
+
+var slicesPerPizza = 10; //given variable
+var people = 25;  //given variable 
+var pizzasOrdered = 5; //given variable
+
+var totalSlices = pizzasOrders * slicesPerPizza; //determines total slices 
+var slicesPerPerson = totalSlices / people;  
+
+console.log("Each person ate" + " " + slicesPerPerson + " " + " slices of pizza at the party")
+
+ 
+
+
+
+
 
 
