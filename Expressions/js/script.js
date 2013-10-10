fe166b0036765bdeef15a3beb7915fb148697223
Josh <mrmyers@fullsail.edu>
2013-10-10 19:25:37 -0400

    concatenating strings

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,6 +1,8 @@
-//Expressions Using Arrays in Expressions
+//Expressions Concatenating Strings
 
-var orangeBins = [244, 567, 883];
-var total= orangeBins[0] + orangeBins [1] + orangeBins [2];
-console.log(total)
+var firstName = "Josh";
+var lastName = "Myers";
+
+var fullName = firstName + " " + lastName; 
+
 
