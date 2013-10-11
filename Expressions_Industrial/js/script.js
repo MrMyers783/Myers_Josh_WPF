1d1ad48fa7d2925f28950c899fc644be65e9f720
Josh <mrmyers@fullsail.edu>
2013-10-10 22:07:51 -0400

    Industrial Expressions

---
+++ Expressions_Industrial/js/script.js
@@ -0,0 +1,16 @@
+// JavaScript 
+//Josh Myers
+// October 10, 2013
+//Expressions - Industrial 
+
+//This project is intended to help a freelance developer manage the productivity of his or her time, as well as how much they made on a particular project vs. the amount of time that they spent on it; the idea being to help manage efficiency, and to better determine what projects are worthwhile moving forward. 
+
+
+var hoursWorked = prompt("Please enter the number of hours you worked on this project:"); 
+var hoursBilled = prompt("Please enter the number of hours you billed for this project:");  
+var payment = prompt("Please enter how much the customer was charged for this project:"); 
+var expense = prompt ("Please enter the cost of any expenses on this project:");  
+alert("You worked" + " " + hoursWorked + " " + "and billed for" + " " + hoursBilled + " " + "and made $" + payment + " " + "on this project." );
+
+
+console.log(hoursWorked)
