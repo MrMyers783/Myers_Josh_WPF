e5c8f446e4ac5ce98c6eef2bcda099cc5a8b4934
Josh <mrmyers@fullsail.edu>
2013-10-10 20:45:16 -0400

    alerts and prompts

---
+++ Alerts_and_prompts/js/script.js
@@ -0,0 +1,10 @@
+//Expressions Alerts and Prompts
+
+var height = prompt ("We are calculating the area of a rectangle. Please enter the height:");
+var width = prompt ("Please enter the rectangle's width:"); 
+var area = height * width; 
+ 
+alert("The area of your rectangle is" + " " + area + " " + "square feet!") 
+ 
+
+
