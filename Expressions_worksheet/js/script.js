68a9b0059ca738dc219939f2d709cd91ef7a6b63
Josh <mrmyers@fullsail.edu>
2013-10-10 20:33:21 -0400

    Discounts
    (First try? Lucky)

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -33,3 +33,17 @@ var total = bills[0] + bills[1] + bills[2] + bills[3] + bills[4]; //provides tot
 var average = total / 5;                                          
 
 console.log("You have spent a total of" + " " + "$" + total + " " + "on groceries over 5 weeks. That is an average of" + " " + "$" + average + " " + "per week.")
+
+//discounts
+
+var price = 100; //original price - given variable 
+var discount = 25; //Discount - given variable
+var desc = "shirt"; //description - given variable 
+var tax = .0625; //sales tax - given variable in decimal for ease of calculation
+
+var perc = .01 //converts discount into decimal for calculation 
+
+var discounted = price - (perc * discount * price);  //price after discount
+var withTax = discounted * tax + discounted; 
+
+console.log ("Your" + " " + desc + " " + "was originally $" + price + ", but after a" + " " + String(discount) + "% discount, it is now $" + discounted + " " + "without tax, and $" + withTax + " " + "with tax.") 
